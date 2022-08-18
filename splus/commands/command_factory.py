b37eaf1a11854f6d3cb16f756c03ef078834acb9
Erik Langille <eriklangille@gmail.com>
2022-08-18 08:57:30 -0400

    Working Command parsing from CLI

--- splus/commands/command_factory.py
+++ splus/commands/command_factory.py
@@ -2,6 +2,7 @@ import argparse
 import inspect
 
 from splus.commands.base_command import BaseCommand
+from splus.commands.command_args import CommandArgs
 from splus.commands.copy_playlist import CopyPlaylist
 from splus.endpoints.factory import Endpoints
 
@@ -14,18 +15,14 @@ class CommandFactory():
     self._add_args(copy_parser, copy)
   
   def parse(self):
-    parsed_args = self._parser.parse_args()
-    func = parsed_args.func
-    params = parsed_args.__dict__
-    del params['func']
-    func(**params) #TODO: Match non-keyword args with right argument
+    command_args = self._parser.parse_args(namespace=CommandArgs())
+    command_args.run_command()
   
   def _add_args(self, parser : argparse.ArgumentParser, command : BaseCommand):
-    sig = inspect.signature(command.run)
-    for parameter_name, parameter in sig.parameters.items():
+    for parameter_name, parameter in command.get_parameters():
       if parameter.annotation == inspect._empty:
         parser.add_argument(parameter_name, type=parameter.annotation)
       else:
         parser.add_argument(f"--{parameter_name}", type=parameter.annotation, default=parameter.default)
-    parser.set_defaults(func=BaseCommand.run)
+    parser.set_defaults(command=command)
     
